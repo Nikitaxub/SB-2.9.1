fce9b560bb8bed5377035e55dd81cacd82790bdb
nikita <nikita.dmd@gmail.com>
2021-11-24 00:29:38 +0300

    Initial Commit

---
+++ SB-2.9.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-2.9.1
+//
+//  Created by xubuntus on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
