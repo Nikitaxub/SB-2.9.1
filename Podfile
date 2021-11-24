f04e56539b2fc5956c7b596d8b3949d5654b12c5
nikita <nikita.dmd@gmail.com>
2021-11-24 23:33:35 +0300

    ready

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '14.0'
+
+target 'SB-2.9.1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SB-2.9.1
+  pod 'Spring', :git => 'https://github.com/LexDeBash/Spring.git'
+
+end
--- SB-2.9.1/ViewController.swift
+++ SB-2.9.1/ViewController.swift
@@ -5,15 +5,66 @@
 //  Created by xubuntus on 24.11.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var avatarView: SpringImageView!
+    
+    @IBOutlet var nextAnimationButton: UIButton!
+    
+    @IBOutlet var infoLabel: UILabel!
+    
+    private let scaleXY: ClosedRange<CGFloat> = 0.5...5
+    private let velocity: ClosedRange<CGFloat> = 0.5...2
+    private let duration: ClosedRange<CGFloat> = 0.2...2
+    
+    private var firstRun = true
+    
+    private var nextPresetName = "\(Spring.AnimationPreset.allCases[Int.random(in: 0...Spring.AnimationPreset.allCases.count - 1)])"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addRandomAnimation()
+        updateInfo()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        nextAnimationButton.layer.cornerRadius = view.frame.width / 40
     }
 
-
+    @IBAction func runNextAnimation() {
+        addRandomAnimation()
+        avatarView.animate()
+        if !firstRun {
+            updateInfo()
+        } else {
+            firstRun.toggle()
+        }
+        nextPresetName = "\(Spring.AnimationPreset.allCases[Int.random(in: 0...Spring.AnimationPreset.allCases.count - 1)])"
+        nextAnimationButton.setTitle("Run \(nextPresetName)", for: .normal)
+    }
+    
+    private func addRandomAnimation() -> Void {
+        let animationCurve = Spring.AnimationCurve.allCases[Int.random(in: 0...Spring.AnimationCurve.allCases.count - 1)]
+        avatarView.animation = nextPresetName
+        avatarView.curve = "\(animationCurve)"
+        avatarView.duration = CGFloat.random(in: duration)
+        avatarView.scaleX = CGFloat.random(in: scaleXY)
+        avatarView.scaleY = CGFloat.random(in: scaleXY)
+        avatarView.velocity = CGFloat.random(in: velocity)
+    }
+    
+    private func updateInfo() -> Void {
+        infoLabel.text = """
+        Animation: \(avatarView.animation)
+        Curve: \(avatarView.curve)
+        Duration: \(String(format: "%.2f", avatarView.duration))
+        ScaleX: \(String(format: "%.2f", avatarView.scaleX))
+        ScaleY: \(String(format: "%.2f", avatarView.scaleY))
+        Velocity: \(String(format: "%.2f", avatarView.velocity))
+        """
+    }
 }
-
